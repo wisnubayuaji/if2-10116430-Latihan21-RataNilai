87184d43450cfa1f5e3a89e8bab246dea4759ecb
wisnubayuaji <wisnubayu34@gmail.com>
2020-11-01 07:28:41 +0700

    Update App.java

--- src/App.java
+++ src/App.java
@@ -1,5 +1,31 @@
+import java.util.Scanner;
+
+/**
+ * @author
+ * Nim  : 10116430
+ * Nama : Wisnu Bayu Aji
+ * Kelas: IF-2
+ */
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        int banyakMahasiswa;
+        double nilaiMahasiswa;
+        double jumlahNilai = 0;
+
+        Scanner bm = new Scanner(System.in);
+        Scanner nm = new Scanner(System.in);
+
+        System.out.print("Masukan Banyaknya Mahasiswa : ");
+        banyakMahasiswa = bm.nextInt();
+
+        for (int i = 1; i <= banyakMahasiswa; i++) {
+            
+            System.out.print("Nilai Mahasiswa ke-" + i + " : ");
+            nilaiMahasiswa = nm.nextInt();
+            jumlahNilai = (jumlahNilai + nilaiMahasiswa);
+        }
+        
+        System.out.println("Maka, Rata-rata Nilainya Adalah " + (jumlahNilai/banyakMahasiswa));
     }
 }
